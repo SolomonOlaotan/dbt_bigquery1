0a99d9c8356149e8b68560dc345a957bd2d73c34
SOLOMON IDOWU <bimsmide@gmail.com>
2024-09-04 22:28:31 +0000

    Cohort analysis

--- models/gold_model/silver_model/customer_analysis/cohortanalysis.sql
+++ models/gold_model/silver_model/customer_analysis/cohortanalysis.sql
@@ -5,16 +5,41 @@ WITH first_purchase AS (
     FROM 
     {{ ref("thelook_orders")}}
     GROUP BY customer_id
-)
+),
+cohorts AS (
 SELECT
-    EXTRACT(YEAR FROM fp.first_order_date) AS cohort_year,
-    EXTRACT(MONTH FROM fp.first_order_date) AS cohort_month,
-    COUNT(DISTINCT o.customer_id) AS retention_count
+    customer_id,
+    EXTRACT(YEAR FROM first_order_date) AS cohort_year,
+    EXTRACT(MONTH FROM first_order_date) AS cohort_month
 FROM 
-    {{ ref("thelook_orders")}} o 
-JOIN 
-    first_purchase fp 
-ON o.customer_id = fp.customer_id 
-WHERE EXTRACT(YEAR FROM o.order_date) = 'cohort_year' AND 
-    EXTRACT(MONTH FROM o.order_date) = 'cohort_month' + 1
-GROUP BY cohort_year, cohort_month
+    first_purchase  
+
+),
+retention AS (
+    SELECT
+        c.customer_id,
+        c.cohort_year,
+        c.cohort_month,
+        EXTRACT(YEAR FROM o.order_date) AS order_year,
+        EXTRACT(MONTH FROM O.order_date) AS order_month
+    FROM 
+        {{ ref("thelook_orders")}} o 
+    JOIN
+    cohort c ON c.customer_id = o.customer_id
+)
+/*Count unique customers making purchases in the month
+following their first purchase*/
+SELECT
+    cohort_year,
+    cohort_month,
+    COUNT(DISTINCT customer_id) AS retention_count
+FROM 
+    retention
+WHERE 
+    (order_year * 12 + order_month) = (cohort_year * 12 + cohort_month +1)
+GROUP BY
+    cohort_year,
+    cohort_month
+ORDER BY 
+    cohort_year,
+    cohort_month
